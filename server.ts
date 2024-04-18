8398cfd8245418482a3f68bdd6daa8d8a788d309
ravifarhan <mravifarhan02@gmail.com>
2024-04-19 06:12:13 +0700

    add package cors

--- server.ts
+++ server.ts
@@ -2,8 +2,9 @@ import express from "express";
 import * as dotenv from "dotenv";
 import db from "./src/db";
 import router from "./src/routes";
-import { follow, getFollowers } from "./src/controller/follow";
+// import { follow, getFollowers } from "./src/controller/follow";
 import path from "path";
+import cors from "cors";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use("/uploads", express.static(path.join(__dirname, "src/uploads")));
 app.use(router);  
 
@@ -24,8 +26,8 @@ app.get("/", async (req, res) => {
   res.send({ listUsers, singleUser });
 });
 
-app.post("/follow", follow);
-app.get("/followers/:followingId", getFollowers);
+// app.post("/follow", follow);
+// app.get("/followers/:followingId", getFollowers);
 
 app.listen(PORT, async () => {
   await db.$connect();
